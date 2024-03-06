98b6c2546f63896ac60dcd80ff237f393fa8d364
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2024-03-06 14:24:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
+const App = () => {
+  const [data, setData] = useState("")
+
+  const fetchData = async () => {
+    let nasa = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`
+      )
+    let nasaData = await nasa.json()
+    console.log(nasaData)
+    setData(nasaData)
+  }
+
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if(!data) {
+    return <h1>loading...</h1>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>nasa API</h1>
+      <img style={{ width: '100%' }} src={data.hdurl} />
+      <h4>{data.explanation}</h4>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
